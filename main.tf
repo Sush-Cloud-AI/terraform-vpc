2f958f8f24ad9e1858ca015e1f422ccf03a3e456
Sush <sushcloud12@gmail.com>
2023-05-29 19:55:21 +0100

    i

--- main.tf
+++ main.tf
@@ -2,5 +2,7 @@ module "vpc" {
   source = "./vendor/modules/vpc"
   VPC_CIDR = var.VPC_CIDR
   ENV = var.ENV
+  SUBNET_CIDR = var.SUBNET_CIDR
+  AZ =var.AZ
 }
 # branch cant be parameterised in source. so we use Terrafile 
