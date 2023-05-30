1649e7bc6166f3936686aee9fa342e650c7cf416
Sush <sushcloud12@gmail.com>
2023-05-30 14:43:36 +0100

    i

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
   source = "./vendor/modules/vpc"
+  DEFAULT_VPC_RT = var.DEFAULT_VPC_RT
   VPC_CIDR = var.VPC_CIDR
   ENV = var.ENV
   SUBNET_CIDR = var.SUBNET_CIDR
   AZ =var.AZ
   DEFAULT_VPC_ID = var.DEFAULT_VPC_ID 
-  DEFAULT_VPC_RT = var.DEFAULT_VPC_RT
+  
   
   
 }
